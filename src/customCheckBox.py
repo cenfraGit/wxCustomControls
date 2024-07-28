06e32fafbd74032092c35220d4c19cf9a322236e
cenfraGit <161994865+cenfraGit@users.noreply.github.com>
2024-07-27 20:58:00 -0600

    Fixed appearance and mouse interaction.
    Added some type annotations.

--- src/customCheckBox.py
+++ src/customCheckBox.py
@@ -8,24 +8,31 @@ class CustomCheckBox(wx.Control):
     
     def __init__(self, parent, id=wx.ID_ANY, label:str="", state=False, pos=wx.DefaultPosition,
                  size=wx.DefaultSize, style=wx.NO_BORDER, validator=wx.DefaultValidator,
-                 name="CustomCheckBox", theme:str="light"):
+                 name="CustomCheckBox", theme:str="light", fontSize=8):
         super().__init__(parent, id, pos, size, style, validator, name)
 
         # control attributes
         self._label = label
         self._Theme = theme # light or dark
         self._Enabled = True
+        self._fontSize = fontSize
 
         # state attributes
         self._state = state # the actual state of the checkbutton
         self.mouseHover = False
+        
+        # control appearance attributes
+        self.checkBoxLeftMargin = dip(5)
+        self.checkBoxTopMargin = dip(5)
+        self.checkBoxSquareSize = dip(15)
+        self.checkBoxSquareSelectedSize = dip(10)
 
         # initialize control colors
         self.initializeColors()
 
-        # set up attributes
-        self.setUpControlAttributes()
-        
+        # initial size
+        self.SetInitialSize(size)
+
         # set up autobufferedpaintdc
         self.SetBackgroundStyle(wx.BG_STYLE_PAINT)
 
@@ -40,7 +47,7 @@ class CustomCheckBox(wx.Control):
         self.Bind(wx.EVT_ENTER_WINDOW, self.OnMouseEnter)
         
         
-    def initializeColors(self):
+    def initializeColors(self) -> None:
         """ Initializes the button's colors according to theme. """
         
         if (self._Theme == "light"):
@@ -52,40 +59,26 @@ class CustomCheckBox(wx.Control):
             self._themeDict = lightTheme
 
 
-    def GetValue(self):
+    def GetValue(self) -> None:
         """ Return the state of the checkbox. """
         return self._state
 
         
-    def SetValue(self, state: bool):
+    def SetValue(self, state: bool) -> None:
         self._state = state
         self.Refresh()
 
 
-    def SetLabel(self, label):
+    def SetLabel(self, label) -> None:
         self._label = label
         self.Refresh()
         
 
-    def OnPaint(self, event):
+    def OnPaint(self, event) -> None:
         """ Handles the paint event. """        
         dc = wx.AutoBufferedPaintDC(self)
         dc.Clear()
         self.Draw(dc)
-
-
-    def paintFirstTimeToDetermineDimensions(self):
-        """ Paints the control one time to get text dimensions """
-        #dc = wx.AutoBufferedPaintDC(self)
-        
-        #dc.SetFont()
-
-
-    def setUpControlAttributes(self):
-        """ Control appearance attributes used in the drawing loop. """
-        self.checkBoxLeftMargin = dip(5)
-        self.checkBoxTopMargin = dip(5)
-        self.checkBoxSquareSize = dip(15)
         
 
     def Draw(self, dc: wx.AutoBufferedPaintDC):
@@ -95,72 +88,122 @@ class CustomCheckBox(wx.Control):
 
         # make backgrund transparent
         dc.SetPen(wx.TRANSPARENT_PEN)
-        #dc.SetBrush(wx.Brush(self.GetParent().GetBackgroundColour(), wx.BRUSHSTYLE_SOLID))
-        dc.SetBrush(wx.Brush(wx.GREEN))
+        dc.SetBrush(wx.Brush(self.GetParent().GetBackgroundColour(), wx.BRUSHSTYLE_SOLID))
+        #dc.SetBrush(wx.Brush(wx.GREEN))
         dc.DrawRectangle(rect)
+
+        # set font
+        dc.SetFont(wx.Font(self._fontSize,
+                           wx.FONTFAMILY_DEFAULT,
+                           wx.FONTSTYLE_NORMAL,
+                           wx.FONTWEIGHT_NORMAL,
+                           faceName=self._themeDict["fontFaceName"]))
+        dc.SetTextForeground(self._themeDict["textForegroundDefault"])
 
         #if not self._Enabled:
 
-        dc.SetPen(wx.Pen(self._themeDict["penDefault"], width=1))
-        dc.SetBrush(wx.Brush(self._themeDict["brushDefault"], wx.BRUSHSTYLE_SOLID))
-        dc.DrawRectangle(self.checkBoxLeftMargin, self.checkBoxTopMargin, self.checkBoxSquareSize, self.checkBoxSquareSize)
+        if not self._Enabled:
+            # draw checkbox
+            dc.SetPen(wx.Pen(self._themeDict["penDisabled"], width=1))
+            dc.SetBrush(wx.Brush(self._themeDict["brushDisabled"], wx.BRUSHSTYLE_SOLID))
+            dc.DrawRectangle(self.checkBoxLeftMargin, self.checkBoxTopMargin, self.checkBoxSquareSize, self.checkBoxSquareSize)
+            # draw label
+            _, textHeight = dc.GetTextExtent(self._label)
+            textX = self.checkBoxLeftMargin*2 + self.checkBoxSquareSize
+            textY = (rect.GetHeight() // 2) - (textHeight//2)
+            dc.SetTextForeground(self._themeDict["textForegroundDisabled"])
+            dc.DrawText(self._label, textX, textY)
 
-        dc.DrawText(self._label, self.checkBoxLeftMargin*2+self.checkBoxSquareSize, 0)
+            if self._state:
+                dc.SetPen(wx.Pen(self._themeDict["penDisabled"], 1))
+                dc.SetBrush(wx.Brush(self._themeDict["brushDisabled"]))
+                # create offsets so that rect is centered
+                leftOffset = self.checkBoxLeftMargin + ((self.checkBoxSquareSize - self.checkBoxSquareSelectedSize) // 2)
+                topOffset = self.checkBoxTopMargin + ((self.checkBoxSquareSize - self.checkBoxSquareSelectedSize) // 2)
+                selectionRect = wx.Rect(leftOffset, topOffset, self.checkBoxSquareSelectedSize, self.checkBoxSquareSelectedSize)
+                dc.DrawRectangle(selectionRect)
 
-        if self._state:
-            dc.SetPen(wx.Pen(self._themeDict["penPressed"], 1))
-            dc.SetBrush(wx.Brush(self._themeDict["brushPressed"]))
+        else:
+            
+
+            dc.SetPen(wx.Pen(self._themeDict["penDefault"], width=1))
+            dc.SetBrush(wx.Brush(self._themeDict["brushDefault"], wx.BRUSHSTYLE_SOLID))
             dc.DrawRectangle(self.checkBoxLeftMargin, self.checkBoxTopMargin, self.checkBoxSquareSize, self.checkBoxSquareSize)
 
+            # draw label
+            _, textHeight = dc.GetTextExtent(self._label)
+            textX = self.checkBoxLeftMargin*2 + self.checkBoxSquareSize
+            textY = (rect.GetHeight() // 2) - (textHeight//2)
+            dc.DrawText(self._label, textX, textY)
+
+            # draw selector
             
+            if self.mouseHover:
 
+                # if user is about to select
+                if not self._state:
+                    dc.SetPen(wx.Pen(self._themeDict["penHover"], 1))
+                    dc.SetBrush(wx.Brush(self._themeDict["brushHover"]))
+                    # create offsets so that rect is centered
+                    leftOffset = self.checkBoxLeftMargin + ((self.checkBoxSquareSize - self.checkBoxSquareSelectedSize) // 2)
+                    topOffset = self.checkBoxTopMargin + ((self.checkBoxSquareSize - self.checkBoxSquareSelectedSize) // 2)
+                    selectionRect = wx.Rect(leftOffset, topOffset, self.checkBoxSquareSelectedSize, self.checkBoxSquareSelectedSize)
+                    dc.DrawRectangle(selectionRect)
 
-        """
-        
-        # check if control is enabled
-        # if not, check if pressed
-        # if not, check if mouse is hovering
-        # if not, draw with default colors
+                # if user is about to de-select
+                else: 
+                    dc.SetPen(wx.Pen(self._themeDict["penPressed"], 1))
+                    dc.SetBrush(wx.Brush(self._themeDict["penPressed"]))
+                    # create offsets so that rect is centered
+                    leftOffset = self.checkBoxLeftMargin + ((self.checkBoxSquareSize - self.checkBoxSquareSelectedSize) // 2)
+                    topOffset = self.checkBoxTopMargin + ((self.checkBoxSquareSize - self.checkBoxSquareSelectedSize) // 2)
+                    selectionRect = wx.Rect(leftOffset, topOffset, self.checkBoxSquareSelectedSize, self.checkBoxSquareSelectedSize)
+                    dc.DrawRectangle(selectionRect)
+                    
+                
+            else:
 
-        if not self._Enabled:
-            dc.SetPen(wx.Pen(self._themeDict["penDisabled"], 1))
-            dc.SetBrush(wx.Brush(self._themeDict["brushDisabled"], wx.BRUSHSTYLE_SOLID))
-            dc.SetTextForeground(self._themeDict["textForegroundDisabled"])
-        else:
-            dc.SetPen(wx.Pen(self._themeDict["penDefault"], 1))
-            dc.SetBrush(wx.Brush(self._themeDict["brushDefault"], wx.BRUSHSTYLE_SOLID))
-            dc.SetTextForeground(self._themeDict["textForegroundDefault"])
-            
-        # draw border
-        dc.DrawRectangle(rect)
-
-        # draw text (to the left)
-        leftMargin = dip(10)
-        dc.SetFont(wx.Font(9, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
-        textWidth, textHeight = dc.GetTextExtent(self._value)
-        textX = leftMargin
-        textY = rect.GetY() + (rect.GetHeight() // 2) - (textHeight // 2)
-        dc.DrawText(self._value, textX, textY)
-        
-        # draw drop down arrow
-        rightMargin = dip(6)
-        width, height = dip(10), dip(4)
-        rectPosX = rect.GetWidth() - width - rightMargin
-        rectPosY = (rect.GetHeight() // 2) - (height // 2)
-        # define icon area
-        arrowRectangle = wx.Rect(rectPosX, rectPosY, width, height)
-        dc.SetPen(wx.Pen(self._themeDict["textForegroundDefault"], 1, wx.PENSTYLE_SOLID))
-        dc.DrawLine(rectPosX+1, rectPosY, rectPosX+(width//2), rectPosY+height)
-        dc.DrawLine(rectPosX+(width//2), rectPosY+height, arrowRectangle.GetTopRight()[0], arrowRectangle.GetTopRight()[1]-1)
-        """
+                if self._state:
+                    
+                    dc.SetPen(wx.Pen(self._themeDict["penPressed"], 1))
+                    dc.SetBrush(wx.Brush(self._themeDict["brushPressed"]))
+                    # create offsets so that rect is centered
+                    leftOffset = self.checkBoxLeftMargin + ((self.checkBoxSquareSize - self.checkBoxSquareSelectedSize) // 2)
+                    topOffset = self.checkBoxTopMargin + ((self.checkBoxSquareSize - self.checkBoxSquareSelectedSize) // 2)
+                    selectionRect = wx.Rect(leftOffset, topOffset, self.checkBoxSquareSelectedSize, self.checkBoxSquareSelectedSize)
+                    dc.DrawRectangle(selectionRect)
 
         
-    def OnEraseBackground(self, event):
+    def OnEraseBackground(self, event) -> None:
         """ Bound to prevent flickering. """
         pass
+
+
+    def DoGetBestClientSize(self) -> wx.Size:
+        """ Determines the best size for the control. """
+
+        # create font
+        font = wx.Font(self._fontSize,
+                       wx.FONTFAMILY_DEFAULT,
+                       wx.FONTSTYLE_NORMAL,
+                       wx.FONTWEIGHT_NORMAL,
+                       faceName=self._themeDict["fontFaceName"])
+
+        # create device context and set font to determine text dimensions
+        dc = wx.ClientDC(self)
+
+        # get text width and height
+        dc.SetFont(font)
+        textWidth, textHeight = dc.GetTextExtent(self._label)
+
+        # dimensions
+        width = (self.checkBoxLeftMargin*2) + self.checkBoxSquareSize + textWidth + dip(5)
+        height = (self.checkBoxTopMargin*2) + max(self.checkBoxSquareSize, textHeight)
+
+        return wx.Size(width, height)    
     
 
-    def OnLeftDown(self, event):
+    def OnLeftDown(self, event) -> None:
         """ Handler for when the checkbox is clicked. """
         
         # invert status
@@ -170,7 +213,7 @@ class CustomCheckBox(wx.Control):
         event.Skip()
 
 
-    def OnLeftUp(self, event):
+    def OnLeftUp(self, event) -> None:
         """ ? """
         if self._state:
             pass
@@ -179,19 +222,19 @@ class CustomCheckBox(wx.Control):
         event.Skip()
         
 
-    def OnMouseLeave(self, event):
+    def OnMouseLeave(self, event) -> None:
         self.mouseHover = False
         self.Refresh()
         event.Skip()
         
 
-    def OnMouseEnter(self, event):
+    def OnMouseEnter(self, event) -> None:
         self.mouseHover = True
         self.Refresh()
         event.Skip()
         
 
-    def Enable(self, enable=True):
+    def Enable(self, enable=True) -> None:
         """
         Uses _Enable to define if the widget is enabled or not,
         instead of default behavior (problems redrawing after modal
@@ -202,7 +245,7 @@ class CustomCheckBox(wx.Control):
         self.Refresh()
         
 
-    def Disable(self):
+    def Disable(self) -> None:
         self.Enable(False)
 
         
@@ -217,6 +260,7 @@ if __name__ == "__main__":
     if (wx.Platform == "__WXMSW__"):
         import ctypes
         ctypes.windll.shcore.SetProcessDpiAwareness(1)
+    from customButton import CustomButton
     
     class MyFrame(wx.Frame):
         def __init__(self):
@@ -228,12 +272,11 @@ if __name__ == "__main__":
             panel = wx.Panel(self)
             panel.SetBackgroundColour(lightTheme["background"] if theme=="light" else blueTheme["background"])
 
-            self.control = CustomCheckBox(panel, label="testing", state=False, pos=wx.Point(50, 50), size=wx.Size(*dip(200, 40)), theme=theme)
+            self.control = CustomCheckBox(panel, label="testing", state=False, pos=wx.Point(50, 50), theme=theme)
+            #self.control.Disable()
 
-
-
-            #self.btn = CustomButton(panel, label="Print value", pos=wx.Point(300, 50), size=wx.Size(140, 40), theme="blue")
-            #self.btn.Bind(wx.EVT_BUTTON, lambda e: print(self.choice.GetValue()))
+            self.btn = CustomButton(panel, label="Print state", pos=wx.Point(300, 50), size=wx.Size(140, 40), theme=theme)
+            self.btn.Bind(wx.EVT_BUTTON, lambda e: print(self.control.GetValue()))
             
 
             self.Show()
